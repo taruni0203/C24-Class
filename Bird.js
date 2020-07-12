6739c9ebc010383ca0ed733fdbd0bc7b48936ac7
taruni0203 <67770771+taruni0203@users.noreply.github.com>
2020-07-11 21:01:30 -0700

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird{
+    constructor(x,y){
+        var options={
+            restitution: 0.5
+        }
+        this.body = Bodies.rectangle(x,y,50,50,options);
+        this.width = 50;
+        this.height =50;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        pos.x = mouseX;
+        pos.y = mouseY;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill(255,0,0);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
